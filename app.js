f3688e5f7f900940d1ffc40c9516570ee89cf12e
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-05-10 18:22:23 -0600

    finished site

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template');
 
 const promptUser = () => {
@@ -129,14 +129,18 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
-
-
-//   Stopped on 9.4.6
--- src/page-template.js
+++ src/page-template.js
@@ -11,7 +11,8 @@ const generateAbout = aboutText => {
       </section>
     `;
   };
-
+  
+  // create the projects section
   const generateProjects = projectsArr => {
     return `
       <section class="my-3" id="portfolio">
@@ -25,7 +26,7 @@ const generateAbout = aboutText => {
               <h3 class="portfolio-item-title text-light">${name}</h3>
               <h5 class="portfolio-languages">
                 Built With:
-                ${languages.join(', ')}
+                ${languages.map(language => language).join(',')}
               </h5>
               <p>${description}</p>
               <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
@@ -55,41 +56,14 @@ const generateAbout = aboutText => {
     `;
   };
   
-    const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(
-      ({ name, description, languages, link }) => {
-        return `
-          <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-            <h3 class="portfolio-item-title text-light">${name}</h3>
-            <h5 class="portfolio-languages">
-              Built With:
-              ${languages.join(', ')}
-            </h5>
-            <p>${description}</p>
-            <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-          </div>
-        `;
-      }
-    );
-  
-    return `
-      <section class="my-3" id="portfolio">
-        <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-        <div class="flex-row justify-space-between">
-        ${featuredProjectHtmlArr.join('')}
-        ${nonFeaturedProjectHtmlArr.join('')}
-        </div>
-      </section>
-    `;
-  };
-
-module.exports = templateData => {
+  // export function to generate entire page
+  module.exports = templateData => {
     // destructure page data by section
     const { projects, about, ...header } = templateData;
   
     return `
     <!DOCTYPE html>
     <html lang="en">
-  
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -99,7 +73,7 @@ module.exports = templateData => {
       <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
       <link rel="stylesheet" href="style.css">
     </head>
-  
+    
     <body>
       <header>
         <div class="container flex-row justify-space-between align-center py-3">
@@ -111,12 +85,10 @@ module.exports = templateData => {
           </nav>
         </div>
       </header>
-
       <main class="container my-5">
-            ${generateAbout(about)}
-            ${generateProjects(projects)}
+        ${generateAbout(about)}
+        ${generateProjects(projects)}
       </main>
-
       <footer class="container text-center py-3">
         <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
       </footer>
